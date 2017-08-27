f77d8921db0a43b61694b9789de1814beab739a7
Matthew Smith <matt@matthewsmith.io>
2017-08-26 20:07:56 -0400

    refactor a bit

--- 02-js-and-css-clock/main.js
+++ 02-js-and-css-clock/main.js
@@ -1,35 +1,37 @@
 (function(){
 
+  // find DOM elements for each hand
   const hourHand = document.querySelector('.hand.hour');
   const minuteHand = document.querySelector('.hand.minute');
   const secondHand = document.querySelector('.hand.second');
 
   let currentTime, hoursAngle, minutesAngle, secondsAngle, seconds, minutes, hours;
 
-  startInterval(function(){
+  // update all hands every second
+  startInterval(setHandsAngle, 1000);
+
+  function setHandsAngle() {
+
     // find the current time
     currentTime = new Date();
 
-    // convert time to fraction of one full rotation around a circle
-    seconds = currentTime.getSeconds()
+    // get time units and make them more precise
+    seconds = currentTime.getSeconds();
     minutes = currentTime.getMinutes() + seconds / 60;
     hours = currentTime.getHours() + minutes / 60;
 
-    secondsAngle = currentTime.getSeconds() * 360 / 60;
+    // compute the angles of each hand
+    secondsAngle = seconds * 360 / 60;
     minutesAngle = (currentTime.getMinutes() + seconds / 60) * 360 / 60;
     hoursAngle = (currentTime.getHours() + minutes / 60) * 360 / 12;
 
-    // set the hands' positions
-    setHandsAngle(hoursAngle, minutesAngle, secondsAngle);
-
-  }, 1000)
-
-  function setHandsAngle(hour, minute, second) {
-    hourHand.style.transform = `rotate(${hour}deg)`;
-    minuteHand.style.transform = `rotate(${minute}deg)`;
-    secondHand.style.transform = `rotate(${second}deg)`;
+    // set the hands' css rotations
+    hourHand.style.transform = `rotate(${hoursAngle}deg)`;
+    minuteHand.style.transform = `rotate(${minutesAngle}deg)`;
+    secondHand.style.transform = `rotate(${secondsAngle}deg)`;
   }
 
+  // modify setInterval to execute the callback immediately instead of after the interval
   function startInterval(callback, time) {
     callback();
     return setInterval(callback, time);
